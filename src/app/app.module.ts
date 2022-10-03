36866a53a03cfcfa04f47ec2d45109c85ee88d36
Polo13410 <polo13410@hotmail.fr>
2022-10-04 00:01:28 +0200

    Adding components
    Adding new components : skills, exp, education
    customizing a bit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { FormationsComponent } from './formations/formations.component';
+import { SkillsComponent } from './skills/skills.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperiencesComponent,
+    FormationsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatSliderModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatExpansionModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/experiences/experiences.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Experience } from 'src/assets/contentInterface'
+
+@Component({
+  selector: 'app-experiences',
+  templateUrl: './experiences.component.html',
+  styleUrls: ['./experiences.component.scss']
+})
+export class ExperiencesComponent {
+  @Input()
+  experiences: Experience[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
