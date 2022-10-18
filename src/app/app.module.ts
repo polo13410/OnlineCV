047f7a7d84326115752a5f10762c52a5e4761389
Polo13410 <polo13410@hotmail.fr>
2022-10-18 20:01:03 +0200

    Adding side bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,12 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +24,7 @@ import { SkillsComponent } from './skills/skills.component';
 import { BubbleComponent } from './bubble/bubble.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
@@ -39,10 +43,10 @@ import { HomepageComponent } from './homepage/homepage.component';
     BrowserAnimationsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'home', component: HomepageComponent},
-      {path: 'edu', component: FormationsComponent},
-      {path: 'exp', component: ExperiencesComponent},
-      {path: 'skills', component: SkillsComponent},
+      { path: 'home', component: HomepageComponent },
+      { path: 'edu', component: FormationsComponent },
+      { path: 'exp', component: ExperiencesComponent },
+      { path: 'skills', component: SkillsComponent },
     ]),
     MatSliderModule,
     MatToolbarModule,
@@ -51,6 +55,9 @@ import { HomepageComponent } from './homepage/homepage.component';
     MatIconModule,
     MatButtonModule,
     MatDividerModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [AppComponent],
   bootstrap: [AppComponent]
