5ced683de9671816f4cb61a8b9bee8c3a23ee69a
Paul Pera <polo13410@hotmail.fr>
2026-05-28 14:57:48 +0200

    feat: experiences page uses MagneticScrollComponent

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -1,41 +1,47 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { ScrollRevealDirective } from '../shared/directives/scroll-reveal.directive';
 import { Subject, takeUntil } from 'rxjs';
-import { Experiences } from 'src/assets/data/contentInterface';
+import { Experience, MagneticScrollItem } from 'src/assets/data/contentInterface';
 import { GetJsonService } from '../services/get-json.service';
+import { MagneticScrollComponent } from '../shared/magnetic-scroll/magnetic-scroll.component';
 
 @Component({
   selector: 'app-experiences',
   templateUrl: './experiences.component.html',
   styleUrl: './experiences.component.scss',
   standalone: true,
-  imports: [CommonModule, TranslateModule, ScrollRevealDirective],
+  imports: [TranslateModule, MagneticScrollComponent],
 })
 export class ExperiencesComponent implements OnInit, OnDestroy {
-  experiences?: Experiences;
-  openPro = new Set<number>();
-  openStage = new Set<number>();
-  private readonly json = inject(GetJsonService);
+  proItems   = signal<MagneticScrollItem[]>([]);
+  stageItems = signal<MagneticScrollItem[]>([]);
+
+  private readonly json     = inject(GetJsonService);
   private readonly destroy$ = new Subject<void>();
 
   ngOnInit(): void {
     this.json.getExp().pipe(takeUntil(this.destroy$)).subscribe(data => {
-      this.experiences = data;
+      this.proItems.set(data.pro.map((e: Experience) => ({
+        title:        e.title,
+        organisation: e.company,
+        location:     e.location,
+        dateFrom:     e.dfrom,
+        dateTo:       e.to,
+        descriptions: e.descriptions,
+      })));
+      this.stageItems.set(data.stage.map((e: Experience) => ({
+        title:        e.title,
+        organisation: e.company,
+        location:     e.location,
+        dateFrom:     e.dfrom,
+        dateTo:       e.to,
+        descriptions: e.descriptions,
+      })));
     });
   }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
-  }
-
-  togglePro(i: number): void {
-    this.openPro.has(i) ? this.openPro.delete(i) : this.openPro.add(i);
-  }
-
-  toggleStage(i: number): void {
-    this.openStage.has(i) ? this.openStage.delete(i) : this.openStage.add(i);
   }
 }
