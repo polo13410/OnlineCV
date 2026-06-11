60bb1b0220f09aea274d1bfd08f3726202bdb3c8
Paul <polo13410@hotmail.fr>
2026-06-11 02:40:25 +0200

    fix(tests): add TranslateModule.forRoot() to AppComponent spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, TranslateModule.forRoot()],
       providers: [provideRouter([])],
     }).compileComponents();
   });
